98977a6b19490c0d60d4e1c85b1e3f103e66fcf1
Dragiša Spasojević <dragisaspasojevic@gmail.com>
2019-01-08 16:56:47 +0100

    Refactor to use a reducer

--- src/providers/TodosProvider/index.js
+++ src/providers/TodosProvider/index.js
@@ -1,53 +1,110 @@
-import React, { useState, createContext } from 'react'
+import React, { useReducer, createContext } from 'react'
 
 export const TodosContext = createContext()
 
+const initialState = {
+  todos: [],
+  autoIncrementingId: 1
+}
+
+function todosReducer(state, action) {
+  switch (action.type) {
+    case 'ADD_TODO': {
+      return {
+        ...state,
+        todos: [
+          ...state.todos,
+          { id: state.autoIncrementingId, ...action.payload }
+        ],
+        autoIncrementingId: state.autoIncrementingId + 1
+      }
+    }
+
+    case 'DELETE_TODO': {
+      return {
+        ...state,
+        todos: state.todos.filter(t => t.id !== action.payload.id)
+      }
+    }
+
+    case 'CHANGE_TODO_PRIORITY': {
+      const todoIndex = state.todos.findIndex(t => t.id === action.payload.id)
+      const newPriority =
+        action.payload.priority !== 3 ? action.payload.priority + 1 : 1
+
+      return {
+        ...state,
+        todos: [
+          ...state.todos.slice(0, todoIndex),
+          { ...action.payload, priority: newPriority },
+          ...state.todos.slice(todoIndex + 1, state.todos.length)
+        ]
+      }
+    }
+
+    case 'MARK_TODO_AS_COMPLETED': {
+      const todoIndex = state.todos.findIndex(t => t.id === action.payload.id)
+
+      return {
+        ...state,
+        todos: [
+          ...state.todos.slice(0, todoIndex),
+          { ...action.payload, isCompleted: true },
+          ...state.todos.slice(todoIndex + 1, state.todos.length)
+        ]
+      }
+    }
+
+    default: {
+      return state
+    }
+  }
+}
+
+const actions = {
+  addTodo: dispatch => todo => {
+    dispatch({
+      type: 'ADD_TODO',
+      payload: todo
+    })
+  },
+  deleteTodo: dispatch => todo => {
+    dispatch({
+      type: 'DELETE_TODO',
+      payload: todo
+    })
+  },
+  changeTodoPriority: dispatch => todo => {
+    dispatch({
+      type: 'CHANGE_TODO_PRIORITY',
+      payload: todo
+    })
+  },
+  markTodoAsCompleted: dispatch => todo => {
+    dispatch({
+      type: 'MARK_TODO_AS_COMPLETED',
+      payload: todo
+    })
+  }
+}
+
+function mapDispatchToActions(dispatch, actions) {
+  return Object.keys(actions).reduce((accumulator, key) => {
+    return {
+      ...accumulator,
+      [key]: actions[key](dispatch)
+    }
+  }, {})
+}
+
 export default function TodosProvider(props) {
-  const [todos, setTodos] = useState([])
-  const [autoIncrementingId, setAutoIncrementingId] = useState(1)
-
-  function addTodo(todo) {
-    setTodos([...todos, { id: autoIncrementingId, ...todo }])
-    setAutoIncrementingId(autoIncrementingId + 1)
-  }
-
-  function deleteTodo(todo) {
-    setTodos(todos.filter(t => t.id !== todo.id))
-  }
-
-  function changeTodoPriority(todo) {
-    const todoIndex = todos.findIndex(t => t.id === todo.id)
-    const newPriority = todo.priority !== 3 ? todo.priority + 1 : 1
-
-    setTodos([
-      ...todos.slice(0, todoIndex),
-      { ...todo, priority: newPriority },
-      ...todos.slice(todoIndex + 1, todos.length)
-    ])
-  }
-
-  function markTodoAsCompleted(todo) {
-    const todoIndex = todos.findIndex(t => t.id === todo.id)
-
-    setTodos([
-      ...todos.slice(0, todoIndex),
-      { ...todo, isCompleted: true },
-      ...todos.slice(todoIndex + 1, todos.length)
-    ])
-  }
+  const [state, dispatch] = useReducer(todosReducer, initialState)
 
   return (
     <TodosContext.Provider
       value={{
-        state: {
-          todos
-        },
-        actions: {
-          addTodo,
-          deleteTodo,
-          changeTodoPriority,
-          markTodoAsCompleted
-        }
+        state,
+        actions: mapDispatchToActions(dispatch, actions)
       }}
     >
       {props.children}
